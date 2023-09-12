be260cf79c07f49ffbcc4f1fd0141896e4f672d2
IdigArtGames <132227781+IdigArtGames@users.noreply.github.com>
2023-09-12 15:22:54 +0200

    DADP ASSIGNMENT 4

--- NavMeshesTut/Assets/Scripts/Player/PlayerMovement.cs
+++ NavMeshesTut/Assets/Scripts/Player/PlayerMovement.cs
@@ -25,11 +25,11 @@ public class PlayerMovement : MonoBehaviour
         var H = Input.GetAxis("Horizontal");
         var V = Input.GetAxis("Vertical");
         velocity.y += gravity * Time.deltaTime;
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(JumpForce * -2f * gravity);
         }
-        controller.Move(velocity* Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
         Vector3 move = transform.right * H + transform.forward * V;
         controller.Move(move * Time.deltaTime * speed);
     }
